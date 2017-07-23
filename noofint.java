b4e03833be319ed7f32a0ad72a2cc88f23be0566
csesuriya <30344091+csesuriya@users.noreply.github.com>
2017-07-23 16:30:30 +0530

    Create noofint.java

---
+++ noofint.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class noofint
+{
+public static void main(String args[])
+{
+int num,cunt=0;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number");
+num=sc.nextInt;
+if(num>0)
+{
+cunt++;
+num=num/10;
+}
+System.out.println("no.of digit="+cunt);
+]
+}
