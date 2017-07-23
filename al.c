9847dbd97b9dde2622de89c3583caeb2f142830c
csesuriya <30344091+csesuriya@users.noreply.github.com>
2017-07-23 09:27:54 +0530

    Create al.c

---
+++ al.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+printf("enter the character");
+scanf("%c",&ch);
+if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z')
+{
+printf("%c is alphabet",ch);
+}
+else
+{
+printf("%c is not alphabet",ch);
+}
+}
